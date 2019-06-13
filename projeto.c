10d53869656b134bddfb8f10c4f775c1ca7e77d3
Marlon <marlom04@hotmail.com>
2019-06-13 20:24:30 -0300

    Funcao multiplicacao

--- projeto.c
+++ projeto.c
@@ -2,13 +2,18 @@
 
 	
 float soma(float a, float b){
-	float soma_num = a + b;
+	float soma_num = a+b;
 	return soma_num;
 }	
 	
 float sub(float a, float b){
-	float sub_num = a - b;
+	float sub_num = a-b;
 	return sub_num;
+}
+
+float mult(float a, float b){
+	float mult_num = a*b;
+	return mult_num;
 }
 	
 	
@@ -17,25 +22,27 @@ int main(){
 	
 	int cod;
 	
-	printf("Digite 1 para somar dois numeros ou 2 para serem subtraidos:\n");
+	printf("Digite 1 para somar\n2 para subtrair\n3 para multiplicar\n");
 	scanf("%d", &cod);
 	
 	switch(cod){
 		case 1:
-			scanf("%f %f", &x, &y);
+			scanf("%f %f", x, y);
 			printf("A soma é %d \n", soma(x,y));
 			break;
 		case 2:
 			scanf("%f %f", &x, &y);
 			printf("A subtracao e %d \n", sub(x,y));
 			break;
-			
-		default:
+		case 3:
+			scanf("%f %f", &x, &y);
+			printf("A multiplicacao e %d \n", mult(x,y));
 			break;
+			
+		default: 
+			printf("Codigo invalido\n");
+			break;	
 	}
-
-	
-
 	
 
 	return 0;
