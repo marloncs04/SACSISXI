dd6a8ab75ae0d7c10ca90ff261c1f4678f9e95e4
Marlon <marlom04@hotmail.com>
2019-06-13 20:01:02 -0300

    Funcao subtraçao

--- projeto.c
+++ projeto.c
@@ -1,20 +1,40 @@
 #include <stdio.h>
 
 	
-int soma(float a, float b){
-	int soma_num = a + b;
+float soma(float a, float b){
+	float soma_num = a + b;
 	return soma_num;
 }	
 	
+float sub(float a, float b){
+	float sub_num = a - b;
+	return sub_num;
+}
 	
 	
 int main(){
 	float x, y;
+	
+	int cod;
+	
+	printf("Digite 1 para somar dois numeros ou 2 para serem subtraidos:\n");
+	scanf("%d", &cod);
+	
+	switch(cod){
+		case 1:
+			scanf("%f %f", &x, &y);
+			printf("A soma é %d \n", soma(x,y));
+			break;
+		case 2:
+			scanf("%f %f", &x, &y);
+			printf("A subtracao e %d \n", sub(x,y));
+			break;
+			
+		default:
+			break;
+	}
 
-	printf("Digite dois numeros para serem somados:\n");
-	scanf("%f %f", &x, &y);
-
-	printf("A soma é %d \n", soma(x,y));
+	
 
 	
 
